e43537f21af965c7233459bfcbb142a1fc6cb555
PostCyberPunk <lyhguest2@outlook.com>
2023-12-28 08:58:34 +0800

    ft ins boot

---
+++ sysins/bootwaitFix.sh
@@ -0,0 +1,3 @@
+# https://young-lord.github.io/posts/arch-to-go
+sudo mkdir /usr/lib/systemd/system-generators-backup
+sudo mv /usr/lib/systemd/system-generators/systemd-gpt-auto-generator /usr/lib/systemd/system-generators-backup/systemd-gpt-auto-generator.bak
---
+++ sysins/systemdBoot.sh
@@ -0,0 +1,8 @@
+bootctl install && echo '!!!Systemd boot installed'
+cp ./arch.conf /boot/loader/entries/ && echo 'copy entry conf'
+cat >>/boot/loader/loader.config <<EOF
+default arch
+timeout 3
+EOF
+nvim -o /etc/fstab /boot/loader/entries/arch.conf
+date
