7c93f298d8d2048411a411b0d7c2c0a3a2052cee
PostCyberPunk <lyhguest2@outlook.com>
2023-12-28 22:12:19 +0800

    ft(git):add gco

--- gitset.sh
+++ gitset.sh
@@ -4,6 +4,11 @@ read -p "email:" umail
 git config --global user.name $uname
 git config --global user.mail $umail
 git config --global init.defaultBranch main
-git config --global credential.credentialStore cache
-git config --global credential.cacheOptions "--timeout 3600"
-git config --global credential.helper git-credential-manager
+
+############gco
+yay -S --needed git-credential-oauth
+git-credential-oauth configure
+############gcm
+# git config --global credential.credentialStore cache
+# git config --global credential.cacheOptions "--timeout 3600"
+# git config --global credential.helper git-credential-manager
