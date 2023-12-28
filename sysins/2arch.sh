041331777c4f36f10f5a83cfa75f59e0d72868c7
PostCyberPunk <lyhguest2@outlook.com>
2023-12-28 23:09:25 +0800

    ft(pst):wget curl

--- sysins/2arch.sh
+++ sysins/2arch.sh
@@ -5,6 +5,6 @@ mkfs.ext4 $mainpart
 mkfs.fat -F 32 $efipart
 mount $mainpart /mnt
 mount --mkdir $efipart /mnt/boot
-pacstrap /mnt base base-devel linux linux-firmware linux-headers fish git neovim lf bottom man-db man-pages networkmanager grub efibootmgr intel-ucode noto-fonts-cjk which lvm2 tumx
+pacstrap /mnt base base-devel linux linux-firmware linux-headers fish git neovim lf bottom man-db man-pages networkmanager grub efibootmgr intel-ucode noto-fonts-cjk which lvm2 tumx wget curl
 genfstab -U /mnt >>/mnt/etc/fstab && echo 'generated fstab'
 date
