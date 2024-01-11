e3b9f8536e6c362739597e9f9e8d3f4b0b1a5b78
PostCyberPunk <lyhguest2@outlook.com>
2024-01-11 15:21:57 +0800

    fix boot fix again

--- sysinit/3systemdBoot.sh
+++ sysinit/3systemdBoot.sh
@@ -2,7 +2,7 @@
 source ./lib/utils.sh
 
 boot_fix() {
-	mkdir /usr/lib/systemd/system-generators-backup
+  mkdir /usr/lib/systemd/system-generators-backup ||(rm -rf /usr/lib/systemd/system-generators-backup&&mkdir /usr/lib/systemd/system-generators-backup)  
 	mv /usr/lib/systemd/system-generators/systemd-gpt-auto-generator /usr/lib/systemd/system-generators-backup/systemd-gpt-auto-generator.bak
 }
 
